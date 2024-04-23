e0370c5e01b8832e33aaaa39f73b288f94eaebaf
NACIMSAAFI <7715@holbertonstudents.com>
2024-04-24 00:34:27 +0100

    task 5. Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,13 +20,15 @@ def stats():
     """
     Return the number of objects by type
     """
-    total_objects = {
-        "amenities": storage.count('Amenity'),
-        "cities": storage.count('City'),
-        "places": storage.count('Place'),
-        "reviews": storage.count('Review'),
-        "states": storage.count('State'),
-        "users": storage.count('User')
-    }
 
-    return jsonify(total_objects)
+
+def get_stats():
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(stats)
