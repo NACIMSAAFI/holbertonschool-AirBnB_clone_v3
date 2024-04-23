7052ca9de6a86c691985c895ec1463ca0ada3525
NACIMSAAFI <7715@holbertonstudents.com>
2024-04-24 00:18:16 +0100

    task 5. Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,23 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route("/status", methods=["GET"])
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def get_status():
     """Returns a JSON with status OK"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
+def get_status():
+    """
+    Return the number of objects by type
+    """
+    total_objects = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+
+    return jsonify(total_objects)
