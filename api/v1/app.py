be6dd744608935a88ee278714d149f165ca16d59
NACIMSAAFI <7715@holbertonstudents.com>
2024-04-25 10:42:01 +0100

    Add a “404 page”, a “Not found”

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ api file """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -9,7 +9,6 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
@@ -18,10 +17,15 @@ def teardown_storage(exception):
     storage.close()
 
 
-if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+@app_views.route("/nop", methods=["GET"], strict_slashes=False)
+def get_nop():
+    """
+    Return “404 page”, a “Not found”
+    """
+    return jsonify({"error": "Not found"})
+
+
+app.register_blueprint(app_views)
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
