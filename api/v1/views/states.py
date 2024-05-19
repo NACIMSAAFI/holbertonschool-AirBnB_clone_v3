db42c5cb9684865087d32bf9deab2ed7d5a0b77f
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-19 21:54:48 +0100

    Coorection code

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -10,14 +10,14 @@ from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get_states():
+def get_all_states():
     """Retrieves the list of all State objects"""
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_state(state_id):
+def get_state_id(state_id):
     """Retrieves a State object"""
     state = storage.get(State, state_id)
     if state is None:
@@ -27,7 +27,7 @@ def get_state(state_id):
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
-def delete_state(state_id):
+def delete_state_id(state_id):
     """Deletes a State object"""
     state = storage.get(State, state_id)
     if state is None:
@@ -38,7 +38,7 @@ def delete_state(state_id):
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-def create_state():
+def create_new_state():
     """Creates a State object"""
     data = request.get_json(silent=True)
     if data is None:
@@ -51,7 +51,7 @@ def create_state():
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def update_state(state_id):
+def update_new_state(state_id):
     """Updates a State object"""
     state = storage.get(State, state_id)
     if state is None:
