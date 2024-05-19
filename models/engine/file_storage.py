82bbf5f8b8841ca5dcdcec32fb861387b0ff6217
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-19 20:48:27 +0100

    Fix code style file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,15 +12,8 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-classes = {
-    "Amenity": Amenity,
-    "BaseModel": BaseModel,
-    "City": City,
-    "Place": Place,
-    "Review": Review,
-    "State": State,
-    "User": User,
-}
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
@@ -52,30 +45,30 @@ class FileStorage:
         json_objects = {}
         for key in self.__objects:
             json_objects[key] = self.__objects[key].to_dict()
-        with open(self.__file_path, "w") as f:
+        with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, "r") as f:
+            with open(self.__file_path, 'r') as f:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except KeyError:
+        except:
             pass
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
         if obj is not None:
-            key = obj.__class__.__name__ + "." + obj.id
+            key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-
+    
     def get(self, cls, id):
         """Retrieve one object based on class and ID."""
         key = f"{cls.__name__}.{id}"
@@ -84,9 +77,6 @@ class FileStorage:
     def count(self, cls=None):
         """Count the number of objects in storage."""
         if cls:
-            return sum(
-                1 for obj_key in self.__objects
-                if obj_key.startswith(cls.__name__)
-            )
+            return sum(1 for obj_key in self.__objects if obj_key.startswith(cls.__name__))
         else:
             return len(self.__objects)
