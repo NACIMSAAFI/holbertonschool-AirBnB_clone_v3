2ee3948dd51444bb0d694772ee635a86be139dab
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-19 19:33:10 +0100

    10. User

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,14 @@
 """
 This module contains the status route
 """
+from api.v1.views import app_views
 from flask import jsonify
-from . import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.user import User
+
 
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
